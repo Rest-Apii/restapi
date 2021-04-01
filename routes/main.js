4fe4d93a501f0ae6dee145dada3146589ab6244b
Rest-Apii <81722042+Rest-Apii@users.noreply.github.com>
2021-04-01 08:13:11 +0700

    Add files via upload

---
+++ routes/main.js
@@ -0,0 +1,31 @@
+__path = process.cwd()
+
+var express = require('express');
+var router = express.Router();
+
+router.get('/', (req, res) => {
+    res.sendFile(__path + '/views/index.html')
+})
+
+router.get('/about', (req, res) => {
+    res.sendFile(__path + '/views/about.html')
+})
+router.get('/docs', (req, res) => {
+    res.sendFile(__path + '/views/docs.html')
+})
+
+router.get('/config', (req, res) => {
+    config = {
+        status: true,
+        result: {
+            prefix : '/',
+            namabot: 'DarkBOT',
+            namaowner: 'Anker',
+            instagram: 'anker_2412',
+            youtube : 'Anker Production'
+        }
+    }
+    res.json(config)
+})
+
+module.exports = router
